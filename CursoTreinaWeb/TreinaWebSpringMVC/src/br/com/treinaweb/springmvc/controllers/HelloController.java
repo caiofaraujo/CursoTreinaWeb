f90eecc53f9dcfb1ad4b9d11fab00410d6959f4f
Caio Fernandes <caio@senac.com>
2017-08-14 20:52:12 -0300

    Curso TreinaWeb Spring MVC

---
+++ CursoTreinaWeb/TreinaWebSpringMVC/src/br/com/treinaweb/springmvc/controllers/HelloController.java
@@ -0,0 +1,16 @@
+ package br.com.treinaweb.springmvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller // Notação para indicar um Controller no contexto do SpringMVC
+@RequestMapping("/hello") // Notação para descrever a URL no contexto do SpringMVC para acessar o controller em questão
+public class HelloController {
+	
+	//localhost:28080/hello/message
+	@RequestMapping("/message")
+	public String message() {
+		return "hello";
+	}
+
+}
