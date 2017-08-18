2aeb339eac54fcff1b1a2c0c4fe4fa284a8a534c
cfaraujo <cfaraujo@STFSAOC024226-W.stefanini.dom>
2017-08-18 15:31:45 -0300

    Repassando informações (mensagem) das actions (controllers) para as views (mensagemDoServidor2) através do objeto ModelAndView

--- CursoTreinaWeb/TreinaWebSpringMVC/src/br/com/treinaweb/springmvc/controllers/HelloController.java
+++ CursoTreinaWeb/TreinaWebSpringMVC/src/br/com/treinaweb/springmvc/controllers/HelloController.java
@@ -1,8 +1,13 @@
 package br.com.treinaweb.springmvc.controllers;
 
+import java.util.Date;
+
+import javax.xml.ws.RequestWrapper;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller // Notação para indicar um Controller no contexto do SpringMVC
 @RequestMapping("/hello") // Notação para descrever a URL no contexto do SpringMVC para acessar o controller em questão
@@ -16,7 +21,14 @@ public class HelloController {
 	
 	@RequestMapping("/mensagemDoServidor")
 	public String mensagemDoServidor(Model model) {
-		model.addAttribute("mensagem", "Olá, TreinaWeb!"); // MENSAGEM SERÁ REPASSADA A VIEW "mensagemDoServidor.jsp" ATRAVÉS DESTE MÉTODO
+		model.addAttribute("mensagem", "Olá, TreinaWeb" + new Date().toString()); // MENSAGEM SERÁ REPASSADA A VIEW "mensagemDoServidor.jsp" ATRAVÉS DESTE MÉTODO
 		return "mensagemDoServidor";
 	}
+	
+	@RequestMapping
+	public ModelAndView mensagemDoServidorV2() {
+		ModelAndView resultado = new ModelAndView("mensagemDoServidor", "mensagem", "Olá TreinaWeb" + new Date().toString());
+		return resultado;
+	}
+	
 }
